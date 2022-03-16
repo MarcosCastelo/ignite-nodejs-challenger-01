d0c9420b5d4ecdda54e84d6de270ede033dbb0bb
MarcosCastelo <marcosvitorcastelo@gmail.com>
2022-03-16 13:59:01 -0300

    [Desafio 01] ensure create and update a todo

--- Desafio 01 - Conceitos do Node.js/src/__tests__/todos.spec.js
+++ Desafio 01 - Conceitos do Node.js/src/__tests__/todos.spec.js
@@ -3,6 +3,77 @@ const { validate } = require('uuid');
 
 const app = require('../');
 
+const createUser = async(name, username) => {
+    const response = await request(app)
+        .post('/users')
+        .send({
+            name,
+            username
+        });
+    return response;
+}
+
+const createTodo = async(title, deadline, username) => {
+    const response = await request(app)
+        .post('/todos')
+        .send({
+            title,
+            deadline
+        })
+        .set('username', username);
+
+    return response;
+}
+
 describe('Todos', () => {
+    it("should be able to create a new todo", async() => {
+        const userResponse = await createUser('John Doe', 'user1');
+
+        const todoDate = new Date();
+        const todoResponse = await createTodo('test todo', todoDate, (await userResponse).body.username)
+        expect(201);
+
+        expect(todoResponse.body).toMatchObject({
+            title: 'test todo',
+            deadline: todoDate.toISOString(),
+            done: false
+        })
+
+        expect(validate(todoResponse.body.id)).toBe(true);
+        expect(todoResponse.body.created_at).toBeTruthy();
+
+    });
+
+    it("should be able to update a todo", async() => {
+        const userResponse = await createUser('John Doe', 'user2');
+        const todoDate = new Date();
+        const todoResponse = await createTodo('test todo', todoDate, userResponse.body.username);
+
+        const updateResponse = await request(app)
+            .put(`/todos/${todoResponse.body.id}`)
+            .send({
+                title: 'update title',
+                deadline: todoDate
+            })
+            .set('username', userResponse.body.username);
+
+        expect(updateResponse.body).toMatchObject({
+            title: 'update title',
+            deadline: todoDate.toISOString(),
+            done: false
+        });
+
+        const getAllTodosResponse = await request(app)
+            .get('/todos')
+            .set('username', userResponse.body.username);
+
+        const updatedTodo = getAllTodosResponse.body.find((todo) => todo.id === todoResponse.body.id)
+
+        expect(updatedTodo).toMatchObject({
+            title: 'update title',
+            deadline: todoDate.toISOString(),
+            done: false
+        })
+    });
 
 })
--- Desafio 01 - Conceitos do Node.js/src/index.js
+++ Desafio 01 - Conceitos do Node.js/src/index.js
@@ -11,7 +11,26 @@ app.use(express.json());
 const users = [];
 
 function checksExistUserAccount(request, response, next) {
-    // TODO
+    const { username } = request.headers;
+
+    const user = users.find(user => user.username === username);
+    if (!user)
+        response.status(400).json({ error: 'User not found' });
+
+    request.user = user;
+    return next();
+}
+
+function checksExistsTodo(request, response, next) {
+    const id = request.params.id;
+    const user = request.user;
+
+    const todo = user.todos.find(todo => todo.id === id)
+    if (!todo)
+        response.status(400).json({ error: 'Todo not found' });
+
+    request.todo = todo;
+    return next();
 }
 
 app.post('/users', (request, response) => {
@@ -35,21 +54,40 @@ app.post('/users', (request, response) => {
 })
 
 app.get('/users', (request, response) => {
-    return response.json({
-        users
-    })
+    return response.json({ users })
 })
 
 app.get('/todos', checksExistUserAccount, (request, response) => {
-    // TODO
+    const user = request.user;
+    response.json(user.todos);
 })
 
 app.post('/todos', checksExistUserAccount, (request, response) => {
-    // TODO
+    const { title, deadline } = request.body;
+    const user = request.user;
+
+    const todo = {
+        id: uuidv4(),
+        created_at: new Date(),
+        title,
+        deadline,
+        done: false
+    }
+
+    user.todos.push(todo);
+    response.status(201).json(todo);
 })
 
-app.put('/todos/:id', checksExistUserAccount, (request, response) => {
-    // TODO
+app.put('/todos/:id', checksExistUserAccount, checksExistsTodo, (request, response) => {
+    const user = request.user;
+    const todo = request.todo;
+    const { title, deadline } = request.body;
+
+    todo.title = title;
+    todo.deadline = deadline;
+
+    response.json(todo)
+
 })
 
 app.patch('/todos/:id/done', checksExistUserAccount, (request, response) => {
